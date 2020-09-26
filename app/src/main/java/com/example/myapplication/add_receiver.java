95b71197df252becc243e41cb2f587faa5d650c3
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2020-09-26 21:13:21 +0530

    first

---
+++ app/src/main/java/com/example/myapplication/add_receiver.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class add_receiver extends AppCompatActivity {
+
+    TextView mTextview, toall, heading, message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_receiver);
+    }}
