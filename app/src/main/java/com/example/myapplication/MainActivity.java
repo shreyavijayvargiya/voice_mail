6484ee4625704649af3a7310b3f4bc945ee182c1
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2020-09-26 21:12:15 +0530

    first

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open();
+
+            }
+        });
     }
-}
+
+    public void open() {
+
+        Intent intent = new Intent(this, add_receiver.class);
+        startActivity(intent);
+    }}
